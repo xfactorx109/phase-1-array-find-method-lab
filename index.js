1d29acdc0b1338c28c2f9bbe843d5819aac1567b
Xavier Humes-Sanchez <xfactorx109@gmail.com>
2024-02-25 20:53:10 -0800

    passing tests

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function superbowlWin(record) {
+    const winningGame = record.find(game => game.result === "W");
+
+    if (winningGame) {
+        return winningGame.year;
+    } else {
+        return undefined;
+    }
+}
